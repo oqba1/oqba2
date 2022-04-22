9ff43197329f6091a456f0bb2a712b9f955aeeb9
oqba <104162003+oqba1@users.noreply.github.com>
2022-04-22 05:20:48 +0300

    https;//oqba.112/vcs

---
+++ ran.lua
@@ -0,0 +1,56 @@
+#! / بن / باش
+cd  $ ( cd $ ( dirname $ 0 ) ؛ pwd )
+صدى -e ' \ e [1m \ e [31m
+يتم تثبيت مصدر BlackPink ،
+الرجاء الانتظار حتى يكتمل التثبيت
+\ 033 [0 د '
+صدى -e ' \ 033 [0 ؛ 33 م
+إذا كانت هذه هي المرة الأولى لتثبيت المصدر ، فأرسل الرقم »{1}
+او اذا كانت اول مره تنصيب السورس ارسل رقم »{1}
+إذا كنت قد قمت بالفعل بتثبيت المصدر ، فأرسل الرقم »{2}
+أو اذا سبق ونصبت السورس ارسل رقم »{2}
+\ 033 [0 د '
+قراءة التثبيت
+حالة  $ التثبيت  في
+1)
+صدى -e ' \ 033 [0؛ 33m بدأ تثبيت Office '
+النوم 2
+sudo apt التحديث
+sudo add-apt-repository ppa: ubuntu-toolchain-r / test
+sudo apt الترقية
+سودو apt-get install redis-server
+sudo apt-get install liblua5.3-dev
+sudo ملائمة تثبيت lua5.3
+sudo apt install zip
+sudo apt install build-basic checkinstall zlib1g-dev -y
+sudo apt install g ++ - 4.7 c ++ - 4.7
+sudo apt install gcc-4.9
+sudo apt Upgrade libstdc ++ 6
+sudo apt install libreadline-dev libconfig-dev libssl-dev lua5.3 liblua5.3-dev lua-socket lua-sec lua-expat libevent-dev قم بفك ضغط git redis-server autoconf g ++ libjansson-dev libpython-dev expat libexpat1-dev
+sudo apt تثبيت الشاشة
+sudo apt install libnotify-dev
+sudo apt تثبيت libconfig ++ 9v5 libstdc ++ 6
+sudo apt تثبيت libconfig ++ 8-dev
+sudo apt install lua-lgi
+sudo apt install build-basic checkinstall zlib1g-dev -y
+مكدير tmp
+القرص المضغوط tmp
+wget https://luarocks.org/releases/luarocks-3.3.0.tar.gz - شهادة no-check-
+القطران zxpf luarocks-3.3.0.tar.gz
+cd luarocks-3.3.0
+./configure && make && sudo make install
+sudo luarocks تثبيت luarocks
+sudo luarocks تثبيت luasec
+sudo luarocks تثبيت luasocket
+القرص المضغوط ../ ..
+rm -rf tmp
+قم بفك ضغط luatele.zip
+sudo lua5.3 BlackPink.lua
+؛؛
+2)
+echo -e " \ e [1m \ e [32m الروبوت قيد التشغيل الآن: جاري تشغيل البوت الان: \ e [0m "
+النوم 2
+قم بفك ضغط luatele.zip
+sudo lua5.3 BlackPink.lua
+؛؛
+esac
